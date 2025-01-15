794cdcef3fb0784912012b47f175c79b684a0c75
AbhilashTUofficial <60460499+AbhilashTUofficial@users.noreply.github.com>
2025-01-15 08:00:02 +0530

    project created

--- cheatsheet/src/app/app.component.spec.ts
+++ cheatsheet/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'cheatsheet' title`, () => {
+  it(`should have the 'secondProject' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cheatsheet');
+    expect(app.title).toEqual('secondProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cheatsheet');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, secondProject');
   });
 });
